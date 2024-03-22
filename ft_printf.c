172766e6ce0901348b8cf73c9f514c9f64cf7357
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-03-22 17:02:18 +0800

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,50 +1,60 @@
-int	hexdecimal_l(unsigned long long n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 16:38:02 by hheng             #+#    #+#             */
+/*   Updated: 2024/03/22 13:07:05 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	hex_lower(unsigned long long n)
 {
-	int	i;
+	int i;
+	char hexa_lower[] = "0123456789abcdef";
 
 	i = 0;
-	if (n >= 16)
+	while (n > 0)
 	{
-		i += hexdecimal_l(n / 16);
-		i += hexdecimal_l(n % 16);
-	}
-	else
-	{
-		if (n < 10)
-			i += ft_putchar(n + 48);
-		else
-			i += ft_putchar(n + 87);
+		int remainder = n % 16;
+		if (remainder < 10)
+			i += ft_putchar(remainder + '0');
+		else 
+			i += ft_putchar(hexa_lower[remainder - 10]);
+		n /= 16;
 	}
 	return (i);
 }
 
-int	hexdecimal_u(unsigned long long n)
+int	hex_upper(unsigned long long n)
 {
-	int	i;
+	int i;
+	char hexa_upper[] = "0123456789ABCDEF";
 
 	i = 0;
-	if (n >= 16)
+	while (n > 0)
 	{
-		i += hexdecimal_u(n / 16);
-		i += hexdecimal_u(n % 16);
-	}
-	else
-	{
-		if (n < 10)
-			i += ft_putchar(n + 48);
-		else
-			i += ft_putchar(n + 55);
+		int remainder = n % 16;
+		if (remainder < 10)
+			i += ft_putchar(remainder + '0');
+		else 
+			i += ft_putchar(hexa_upper[remainder - 10]);
+		n /= 16;
 	}
 	return (i);
 }
 
-int	printf_pointer(unsigned long long p)
+int	print_pointer(unsigned long long p)
 {
 	int	i;
 
 	i = 0;
 	i += ft_putstr("0x");
-	i += hexdecimal_l(p);
+	i += hex_lower(p);
 	return (i);
 }
 
@@ -71,24 +81,42 @@ int	printf_specifiers(va_list args, char format, int a)
 	return (a);
 }
 
-int	ft_printf(const char *format, ...)
-{
-	int		a;
-	va_list	args;
+// int	ft_printf(const char *format, ...)
+// {
+// 	int		a;
+// 	va_list	args;
 
-	va_start(args, format);
-	a = 0;
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			a = printf_specifiers(args, *format, a);
-		}
-		else
-			a += write(1, format, 1);
-		format++;
-	}
-	va_end(args);
-	return (a);
+// 	va_start(args, format);
+// 	a = 0;
+// 	while (*format != '\0')
+// 	{
+// 		if (*format == '%')
+// 		{
+// 			format++;
+// 			a = printf_specifiers(args, *format, a);
+// 		}
+// 		else
+// 			a += write(1, format, 1);
+// 		format++;
+// 	}
+// 	va_end(args);
+// 	return (a);
+// }
+// */
+
+   int main() {
+    // Test value
+    unsigned long long test_value = 27;
+
+    // Print lowercase hexadecimal representation
+    printf("Lowercase hexadecimal representation: ");
+    hex_lower(test_value);
+    printf("\n");
+
+    // Print uppercase hexadecimal representation
+    printf("Uppercase hexadecimal representation: ");
+    hex_upper(test_value);
+    printf("\n");
+
+    return 0;
 }
