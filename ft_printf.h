99681d40469d4020866a984a8da96a9a2229342a
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-04-09 18:56:21 +0800

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 19:53:45 by hheng             #+#    #+#             */
-/*   Updated: 2024/03/22 17:09:40 by hheng            ###   ########.fr       */
+/*   Updated: 2024/04/09 18:48:21 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	ft_putchar(int c);
 int	ft_putstr(char *str);
 int	ft_putnbr(int n);
 int	ft_putnbr_u(unsigned int n);
-int	hex_lower(unsigned long long n);
-int	hex_upper(unsigned long long n);
+int	printf_hex_l(unsigned long long n);
+int	printf_hex_u(unsigned long long n);
 int	printf_pointer(unsigned long long p);
 int	printf_specifiers(va_list args, char format, int a);
 int	ft_printf(const char *format, ...);
