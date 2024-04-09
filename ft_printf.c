b024b0ee7cee12db9ca3d96ec6f22b7e7da963d5
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-04-09 18:55:30 +0800

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,56 +6,60 @@
 /*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 16:38:02 by hheng             #+#    #+#             */
-/*   Updated: 2024/03/22 17:12:43 by hheng            ###   ########.fr       */
+/*   Updated: 2024/04/09 18:49:01 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	hex_lower(unsigned long long n)
+int	printf_hex_l(unsigned long long n)
 {
-	int i;
-	char hexa_lower[] = "0123456789abcdef";
+	int charCount = 0;
 
-	i = 0;
-	while (n > 0)
-	{
-		int remainder = n % 16;
-		if (remainder < 10)
-			i += ft_putchar(remainder + '0');
-		else 
-			i += ft_putchar(hexa_lower[remainder - 10]);
-		n /= 16;
-	}
-	return (i);
+    if (n >= 16) {
+        charCount += printf_hex_l(n / 16);
+        charCount += printf_hex_l(n % 16);
+    }
+
+    else {
+        if (n < 10) {
+            charCount += ft_putchar(n + '0');
+        }
+        else {
+            charCount += ft_putchar(n + 'a' - 10);
+        }
+    }
+    return charCount;
 }
 
-int	hex_upper(unsigned long long n)
+int	printf_hex_u(unsigned long long n)
 {
-	int i;
-	char hexa_upper[] = "0123456789ABCDEF";
+	int charCount = 0;
 
-	i = 0;
-	while (n > 0)
-	{
-		int remainder = n % 16;
-		if (remainder < 10)
-			i += ft_putchar(remainder + '0');
-		else 
-			i += ft_putchar(hexa_upper[remainder - 10]);
-		n /= 16;
-	}
-	return (i);
+    if (n >= 16) {
+        charCount += printf_hex_u(n / 16);
+        charCount += printf_hex_u(n % 16);
+    }
+    else {
+        if (n < 10) {
+            charCount += ft_putchar(n + '0');
+        }
+        else {
+            charCount += ft_putchar(n + 'A' - 10);
+        }
+    }
+    return charCount;
 }
 
-int	print_pointer(unsigned long long p)
+int	printf_pointer(unsigned long long p)
 {
-	int	i;
+	int charCount = 0;
 
-	i = 0;
-	i += ft_putstr("0x");
-	i += hex_lower(p);
-	return (i);
+    charCount += ft_putstr("0x");
+
+    charCount += printf_hex_l(p);
+
+    return charCount;
 }
 
 int	printf_specifiers(va_list args, char format, int a)
@@ -73,11 +77,11 @@ int	printf_specifiers(va_list args, char format, int a)
 	else if (format == 'u')
 		a += ft_putnbr_u(va_arg(args, unsigned int));
 	else if (format == 'x')
-		a += hex_lower(va_arg(args, unsigned int));
+		a += printf_hex_l(va_arg(args, unsigned int));
 	else if (format == 'X')
-		a += hex_upper(va_arg(args, unsigned int));
+		a += printf_hex_u(va_arg(args, unsigned int));
 	else if (format == 'p')
-		a += print_pointer(va_arg(args, unsigned long long));
+		a += printf_pointer(va_arg(args, unsigned long long));
 	return (a);
 }
 
@@ -102,56 +106,4 @@ int	ft_printf(const char *format, ...)
 	va_end(args);
 	return (a);
 }
-int main() {
-    int printed_chars;
-
-    // Testing various format specifiers
-    printed_chars = ft_printf("Testing ft_printf:\n");
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    printed_chars = ft_printf("Integer (d): %d\n", 123);
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    printed_chars = ft_printf("String (s): %s\n", "Hello, world!");
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    printed_chars = ft_printf("Character (c): %c\n", 'A');
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    printed_chars = ft_printf("Percentage (%%): %%\n");
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    printed_chars = ft_printf("Unsigned Integer (u): %u\n", 456);
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    printed_chars = ft_printf("Hexadecimal (x): %x\n", 255);
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    printed_chars = ft_printf("Hexadecimal (X): %X\n", 255);
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    // Testing pointer specifier
-    int *ptr = NULL;
-    printed_chars = ft_printf("Pointer (p): %p\n", (void *)ptr);
-    printf("Number of characters printed: %d\n\n", printed_chars);
-
-    return 0;
-}
-
-//    int main() {
-//     // Test value
-//     unsigned long long test_value = 27;
-
-//     // Print lowercase hexadecimal representation
-//     printf("Lowercase hexadecimal representation: ");
-//     hex_lower(test_value);
-//     printf("\n");
-
-//     // Print uppercase hexadecimal representation
-//     printf("Uppercase hexadecimal representation: ");
-//     hex_upper(test_value);
-//     printf("\n");
-
-//     return 0;
-// }
 
